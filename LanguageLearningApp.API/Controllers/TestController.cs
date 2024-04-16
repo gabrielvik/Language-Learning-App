3d3b4341d186f74915079de3470dad6bff711fdb
lester <gabriel.vikstrom@gmail.com>
2024-04-16 16:12:18 +0200

    Add project files.

---
+++ LanguageLearningApp.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageLearningApp.API.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("TestController")]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
---
+++ LanguageLearningApp.API/Program.cs
@@ -0,0 +1,40 @@
+namespace LanguageLearningApp.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = "swagger";
+                    options.DocumentTitle = "LanguageApp Swagger";
+                });
+            }
+
+            app.Run();
+        }
+    }
+}
