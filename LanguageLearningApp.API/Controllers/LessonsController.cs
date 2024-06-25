1aa9527a907ae23803fab149fbf6cde98545cd6b
lester <gabriel.vikstrom@gmail.com>
2024-06-25 15:38:36 +0200

    some basic lesson code Backend

---
+++ LanguageLearningApp.API/Controllers/LessonsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LanguageLearningApp.Data.Entities; // Ensure to include this namespace
+
+namespace LanguageLearningApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LessonsController : ControllerBase
+    {
+        [HttpGet("lesson")]
+        public IActionResult GetLesson(int id)
+        {
+            var lesson = Lessons.GetLesson(id);
+
+            if (lesson != null)
+            {
+                return Ok(lesson); 
+            }
+
+            return BadRequest("Lesson not found.");
+        }
+    }
+}
---
+++ LanguageLearningApp.Data/Entities/Lessons.cs
@@ -0,0 +1,24 @@
+﻿using LanguageLearningApp.Data.Lessons;
+
+namespace LanguageLearningApp.Data.Entities
+{
+    public static class Lessons
+    {
+        public static Dictionary<int, List<Lesson>> lessons = new Dictionary<int, List<Lesson>>();
+
+        static Lessons()
+        {
+            InitializeLessons();
+        }
+
+        private static void InitializeLessons()
+        {
+            lessons.Add(1, Greetings.greetings);
+        }
+
+        public static List<Lesson>? GetLesson(int id)
+        {
+            return lessons.ContainsKey(id) ? lessons[id] : null;
+        }
+    }
+}
